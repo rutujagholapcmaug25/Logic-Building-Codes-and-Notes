cfe9e75b670a436f90004a4cbbdeb201ebb27671
Rutuja Pravin Gholap <rutuja.gholap.cmaug25@gmail.com>
2025-09-03 08:47:46 +0530

    Delete DAY-1/Square.java

--- DAY-1/Square.java
+++
@@ -1,7 +0,0 @@
-class Square {
-    public static void main(String[] args) {
-        int side = 4;
-        int area = side * side;
-        System.out.println("Area of Square = " + area);
-    }
-}
